cfbcc440ad1f9d7612ce68015e4b1fc283140f64
chenguocaiyx <ubuntu@localhost.localdomain>
2018-05-08 13:46:26 +0800

    站点更新:2018-05-08 13/46/26

---
+++ urls.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: urls.sh
+# Author: ma6174
+# mail: ma6174@163.com
+# Created Time: Tue 08 May 2018 01:32:05 PM CST
+#########################################################################
+#!/bin/bash
+
+curl -H 'Content-Type:text/plain' --data-binary @~/hexo/public/baidu_urls.txt "http://data.zz.baidu.com/urls?site=gcchen.cn&token=x0SPBnwbE0swXSlg"
+
